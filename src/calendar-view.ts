d5bd0cc2762be13f76f5af2dfacbbe5fc648bef9
Codeacula <codeacula@codeacula.com>
2025-11-06 12:38:53 -0600

    Populate calendar with events from Bases data and refresh on data updates

--- src/calendar-view.ts
+++ src/calendar-view.ts
@@ -23,12 +23,33 @@ export class CalendarView extends BasesView {
 			this.calendarEl,
 			[DayGrid]
 		);
+		this.updateCalendarEvents();
 	}
 
 	onDataUpdated(): void {
+		// TODO: Clear the existing events
+		this.updateCalendarEvents();
 	}
 
 	private destruct(): void {
 		destroyCalendar(this.calendarInstance);
+	}
+
+	private updateCalendarEvents(): void {
+
+		// TODO: Filter out notes that aren't in the current time period
+		console.log("AAAAHHH", this.data);
+
+		if (!this.data) {
+			return;
+		}
+
+		for (const entry of this.data.data) {
+			this.calendarInstance.addEvent({
+				title: entry.file.name,
+				start: entry.getValue('file.due-date'),
+				end: entry.getValue('file.due-date')
+			});
+		}
 	}
 }
